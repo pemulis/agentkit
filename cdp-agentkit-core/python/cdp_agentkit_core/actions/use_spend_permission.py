0b6a53cf47abc590f816c03e24a43d588ef8ccfc
Rohan Agarwal <rohan.agarwal.1@coinbase.com>
2025-01-22 17:53:28 -0500

    demo

--- cdp-agentkit-core/python/cdp_agentkit_core/actions/use_spend_permission.py
+++ cdp-agentkit-core/python/cdp_agentkit_core/actions/use_spend_permission.py
@@ -60,26 +60,58 @@ def use_spend_permission(wallet: Wallet, account: str, token: str, allowance: st
         str: A message containing the spend permission details.
 
     """
-    BASE_SEPOLIA_SPEND_PERMISSIONS_CONTRACT_ADDRESS = "0xf85210B21cC50302F477BA56686d2019dC9b67Ad"
+    BASE_MAINNET_SPEND_PERMISSIONS_CONTRACT_ADDRESS = "0xf85210B21cC50302F477BA56686d2019dC9b67Ad"
+    
+    SPEND_FUNCTION_ABI = [{
+        "inputs": [
+            {
+                "components": [
+                    {"internalType": "address", "name": "account", "type": "address"},
+                    {"internalType": "address", "name": "spender", "type": "address"},
+                    {"internalType": "address", "name": "token", "type": "address"},
+                    {"internalType": "uint160", "name": "allowance", "type": "uint160"},
+                    {"internalType": "uint48", "name": "period", "type": "uint48"},
+                    {"internalType": "uint48", "name": "start", "type": "uint48"},
+                    {"internalType": "uint48", "name": "end", "type": "uint48"},
+                    {"internalType": "uint256", "name": "salt", "type": "uint256"},
+                    {"internalType": "bytes", "name": "extraData", "type": "bytes"}
+                ],
+                "internalType": "struct SpendPermissionManager.SpendPermission",
+                "name": "spendPermission",
+                "type": "tuple"
+            },
+            {
+                "internalType": "uint160",
+                "name": "value",
+                "type": "uint160"
+            }
+        ],
+        "name": "spend",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }]
+    
     try:        
         spend_permission_transaction = wallet.invoke_contract(
-            contract_address=BASE_SEPOLIA_SPEND_PERMISSIONS_CONTRACT_ADDRESS,
+            contract_address=BASE_MAINNET_SPEND_PERMISSIONS_CONTRACT_ADDRESS,
             method="spend",
             args={
-                "spendPermission": {
-                    "account": account,
-                    "spender": wallet.default_address.address_id,
-                    "token": token,
-                    "allowance": allowance,
-                    "period": period,
-                    "start": start,
-                    "end": end,
-                    "salt": "0",
-                    "bytes": "0x",
-                },
-                "amount": amount,
+                "spendPermission": [
+                    account,
+                    wallet.default_address.address_id,
+                    token,
+                    allowance,
+                    period,
+                    start,
+                    end,
+                    "3",
+                    "0x",
+                ],
+                "value": amount,
             },
-            amount=amount,
+            abi=SPEND_FUNCTION_ABI,
+            amount=0,
         )
         
         spend_permission_transaction.wait()
