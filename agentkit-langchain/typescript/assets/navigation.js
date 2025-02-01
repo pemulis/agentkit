12ae66b204db45c2ff395a717a595239553a1cf4
John-peterson-coinbase <John-peterson-coinbase@users.noreply.github.com>
2025-02-01 13:58:32 +0000

    deploy: 9bd31c027705eacc3c689395f3f953d8d83a878b

---
+++ agentkit-langchain/typescript/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "eJx1jsEKwjAQRP9lzqGCiIdcvXrsrfQQmrVZmm7EbKFQ8u+i0EoVrzPDm9csUJoVFiyeZhjcnQZYjMlPkfLhHVdBxwiDgcXDHg26wNE/SGCbDdCTXp30l+BY6pRi/sBuk3TKSVbcz3R/cD6Vtpi9WaWU9Z/eq/tyLO0TV95Liw=="
